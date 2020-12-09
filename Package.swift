e48e207c8d7bb3ade22c95b5d14ddaaa6b43dd40
Yannik Ehlert <yannik.ehlert@claas.com>
2020-12-09 19:10:01 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SendBirdUIKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SendBirdUIKit",
+            targets: ["SendBirdUIKit"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .binaryTarget(name: "SendBirdUIKit", url: "https://github.com/", checksum: "0000000000000000000000000000000000000000000000000000000000000000"),
+    ]
+)
