5aaaed1d195dccec3ff8f89e146953fc5b0b2658
Microsoft.TeamFoundation.System <>
2022-02-15 08:00:35 +0100

    SendBirdUIKit v2.2.4

--- Package.swift
+++ Package.swift
@@ -15,6 +15,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
     ],
     targets: [
-        .binaryTarget(name: "SendBirdUIKit", url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v2.2.3/SendBirdUIKit.xcframework.zip", checksum: "50b7f27b79488c1438a7f1c92d90db09a13dba2b9d9b869dc24c53549824fc0f"),
+        .binaryTarget(name: "SendBirdUIKit", url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v2.2.4/SendBirdUIKit.xcframework.zip", checksum: "14ef723ac0b5fb146ced0ff9bc8e97b952035c73db75fc875917ba91de49e1f4"),
     ]
 )
